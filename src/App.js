ff85d843504a0a1c8690d6efab5c817c29a09e2f
harsh <harshmangat0007@gmail.com>
2021-09-03 16:07:09 +0530

    upgrade-features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [cartIsShown, setCartIsSShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsSShown(true);
+  };
+
+  const  hideCartHandler = () => {
+    setCartIsSShown(false);
+  };
+
   return (
     <Fragment>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
